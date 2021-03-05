3182793753a6bea1b83f3f90482f275ab13b2fe4
Roman4ick <roman4ick.suvorov@yandex.ru>
2021-03-05 20:17:12 +0300

    защита cors

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+app.use(cors());
 
 const routerarticle = require('./routes/article');
 const routerusers = require('./routes/users');
