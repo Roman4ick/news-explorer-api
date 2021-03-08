ca46a26b05961203df7549beff5fd737e3e13ac0
Roman4ick <62401079+Roman4ick@users.noreply.github.com>
2021-03-08 16:21:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.post('/signup', celebrate({
     name: Joi.string().required().min(2).max(30),
   }),
 }), createUser);
-app.use(routerarticle);
 app.use(auth);
+app.use(routerarticle);
 app.use(routerusers);
 app.use('/', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден!');
