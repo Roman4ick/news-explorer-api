f5d1ca0e5b31ffa5ea7d564f6f0baa6acd3a6f5f
Roman4ick <62401079+Roman4ick@users.noreply.github.com>
2021-03-08 16:23:13 +0300

    Update article.js

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,11 @@ Joi.objectId = require('joi-objectid')(Joi);
 const routerarticle = require('express').Router();
 const { getArticle, deleteArticle, createArticle } = require('../controllers/article');
 
-routerarticle.get('/articles', getArticle);
+routerarticle.get('/articles', celebrate({
+  headers: Joi.object().keys({
+    authorization: Joi.string().required(),
+  }).unknown(true),
+}), getArticle);                                                                  
 routerarticle.delete('/articles/:Id', celebrate({
   params: Joi.object().keys({
     Id: Joi.objectId().hex().length(24),
